69c798a8d9c5ea9f01dc1be460f10da06ef6ad96
dvanhecke <duartevanhecke@gmail.com>
2024-12-07 11:34:05 +0100

    day 7

--- adventofcode/src/d07.py
+++ adventofcode/src/d07.py
@@ -4,8 +4,46 @@ Run it with the command `python -m adventofcode run_solution 7` from the project
 """
 
 from adventofcode.types import Solution
+from itertools import product
 
+def find_operations(numbers, target, use_concat=False):
+    n = len(numbers)
+    if n < 2:
+        return []
+
+    operators = ["+", "*"]
+    if use_concat:
+        operators.append("||")
+
+    # Generate all combinations of operators
+    operator_combinations = product(operators, repeat=n-1)
+    state = False
+
+    for ops in operator_combinations:
+        result = numbers[0]
+        expression = str(numbers[0])
+        
+        for i, op in enumerate(ops):
+            if op == "+":
+                result += numbers[i+1]
+            elif op == "*":
+                result *= numbers[i+1]
+            elif op == "||":
+                # Concatenate as strings, then convert to integer
+                result = int(str(result) + str(numbers[i+1]))
+            expression += f" {op} {numbers[i+1]}"
+        
+        if result == target:
+            state = True
+            break
+
+    return state
 
 def run(data: str) -> Solution:
-    # not yet implemented!
-    return (None, None)
+    results = [int(line.split(": ")[0]) for line in data.split("\n")]
+    values = [[int(char) for char in line.split(": ")[1].split(" ")] for line in data.split("\n")]
+    calibration = [find_operations(values[idx], res) for idx, res in enumerate(results)]
+    calibration2 = [find_operations(values[idx], res, True) for idx, res in enumerate(results)]
+    final_sum = sum([res for idx, res in enumerate(results) if calibration[idx]])
+    final_sum2 = sum([res for idx, res in enumerate(results) if calibration2[idx]])
+    return (final_sum, final_sum2)
--- adventofcode/tests/test_2024_07.py
+++ adventofcode/tests/test_2024_07.py
@@ -7,5 +7,13 @@ from adventofcode.src.d07 import run
 
 
 def test_run() -> None:
-    # not yet implemented!
-    assert run("") == (None, None)
+    test_data = """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
+    assert run(test_data) == (3749, 11387)
