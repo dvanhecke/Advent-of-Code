21aa045a1b152d0013396b12528451e22cc5c471
dvanhecke <duartevanhecke@gmail.com>
2024-12-12 21:09:04 +0100

    day 12

--- adventofcode/tests/test_2024_12.py
+++ adventofcode/tests/test_2024_12.py
@@ -7,5 +7,14 @@ from adventofcode.src.d12 import run
 
 
 def test_run(benchmark) -> None:
-    test_data = ""
-    assert benchmark(run, test_data) == (None, None)
+    test_data = """RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"""
+    assert benchmark(run, test_data) == (1930, 1206)
