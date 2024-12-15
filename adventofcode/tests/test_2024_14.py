24131711467273c72de238a298b2a92a1cc37276
dvanhecke <duartevanhecke@gmail.com>
2024-12-15 01:30:46 +0100

    day 14

--- adventofcode/tests/test_2024_14.py
+++ adventofcode/tests/test_2024_14.py
@@ -7,5 +7,16 @@ from adventofcode.src.d14 import run
 
 
 def test_run(benchmark) -> None:
-    test_data = ""
-    assert benchmark(run, test_data) == (None, None)
+    test_data = """p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"""
+    assert benchmark(run, test_data, True) == (12, 24)
